971fed3a633bc6d904b3a5f9db8451cd93a846c0
SimonOliveros <s.oliverosa@uniandes.edu.co>
2023-09-25 23:50:22 -0500

    Subida ejercicio 3

--- Taller_2/Punto_3_GL_GH.py
+++ Taller_2/Punto_3_GL_GH.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sym
+import matplotlib.pyplot as plt
 
 x = sym.Symbol('x',real=True)
 y = sym.Symbol('y',real=True)
@@ -11,7 +12,7 @@ def GetLaguerreRecursive(n,x):
     elif n==1:
         poly = 1-x
     else:
-        poly = ((2*n-1-x)*GetLaguerreRecursive(n-1,x)-(n-1)*GetLaguerreRecursive(n-2,x))/n
+        poly = ((2*(n-1)+1-x)*GetLaguerreRecursive(n-1,x)-(n-1)*GetLaguerreRecursive(n-2,x))/n
    
     return sym.expand(poly,x)
 
@@ -63,9 +64,9 @@ def GetRoots(f,df,x,tolerancia = 10):
     return Roots
 
 def GetAllRootsGLag(n):
-    N=n+(n+1)*n**(1/2)
+    N=n+(n-1)*n**(1/2)
 
-    xn = np.linspace(0,N,100)
+    xn = np.linspace(0,N,300)
     
     Laguerre = []
     DLaguerre = []
@@ -82,6 +83,65 @@ def GetAllRootsGLag(n):
         ValueError('El número de raíces debe ser igual al n del polinomio.')
     
     return Roots
+
+def GetWeightsGLag(n):
+
+    Roots = GetAllRootsGLag(n)
+    
+
+    PolyLaguerre= sym.lambdify([x],GetLaguerreRecursive(n+1,x),'numpy')
+
+
+
+    Weights = Roots/(((n+1)**2)*(PolyLaguerre(Roots)**2))
+    
+    return Weights
+
+
+def GetHermiteRecursive(n,x):
+
+    if n==0:
+        poly = sym.Number(1)
+    elif n==1:
+        poly = 2*x
+    else:
+        poly = (2*x*GetHermiteRecursive(n-1,x))-(2*(n-1)*GetHermiteRecursive(n-2,x))
+   
+    return sym.expand(poly,x)
+
+
+
+
+def P(v,T,m,R):
+    
+    a=(m/(2*np.pi*R*T))**(3/2)
+    b=v**2
+    e=-((m*v**2)/(2*np.pi*R*T))
+    p=4*np.pi
+    return p*a*b*np.exp(e)
+
+v=np.linspace(0,600,601)
+
+for t in range(100,1100,100):
+    y=P(v,t,1,1)
+    x=v
+
+    plt.scatter(x,y)
+    plt.xlabel("Velocidad")
+    plt.ylabel("Probabilidad")
+    plt.title("Distribución de las velocidades de las moléculas de un gas ideal \n  para distintas temperaturas")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
